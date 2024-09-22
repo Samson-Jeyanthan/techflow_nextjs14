8c7145a4d7c82e91619e5e8d98fa9e84f603e311
Samson <samsonjeyanthan5@gmail.com>
2024-09-22 18:44:45 +0530

    Added tags function to the question form and text editor updates

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -16,9 +16,11 @@ import { Input } from "@/components/ui/input";
 import { Button } from "../ui/button";
 import { QuestionsSchema } from "@/lib/validations";
 import { Editor } from "@tinymce/tinymce-react";
-import { useRef } from "react";
+import React, { useRef } from "react";
+import { Badge } from "../ui/badge";
+import { IoClose } from "react-icons/io5";
 
-function QuestionForm() {
+const QuestionForm = () => {
   const editorRef = useRef(null);
 
   const form = useForm<z.infer<typeof QuestionsSchema>>({
@@ -30,9 +32,44 @@ function QuestionForm() {
     },
   });
 
+  const handleInputKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    field: any
+  ) => {
+    if (e.key === "Enter" && field.name === "tags") {
+      e.preventDefault();
+
+      const tagInput = e.target as HTMLInputElement;
+      const tagValue = tagInput.value.trim();
+
+      if (tagValue !== "") {
+        if (tagValue.length > 15) {
+          return form.setError("tags", {
+            type: "required",
+            message: "Tags must be less than 15 characters",
+          });
+        }
+
+        if (!field.value.includes(tagValue as never)) {
+          form.setValue("tags", [...field.value, tagValue]);
+          tagInput.value = "";
+          form.clearErrors("tags");
+        } else {
+          form.trigger();
+        }
+      }
+    }
+  };
+
+  const handleTagRemove = (tag: string, field: any) => {
+    const newTags = field.value.filter((t: string) => t !== tag);
+    form.setValue("tags", newTags);
+  };
+
   function onSubmit(values: z.infer<typeof QuestionsSchema>) {
     console.log(values);
   }
+
   return (
     <Form {...form}>
       <form
@@ -123,10 +160,31 @@ function QuestionForm() {
             <FormItem className="flex w-full flex-col">
               <FormLabel className="text-dark-100_light-850">Tags</FormLabel>
               <FormControl className="mt-3.5">
-                <Input
-                  {...field}
-                  className="no-focus text-dark-100_light-850 border border-solid border-light-750 bg-light-800 text-sm dark:border-dark-350 dark:bg-dark-250"
-                />
+                <>
+                  <Input
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
+                    className="no-focus text-dark-100_light-850 border border-solid border-light-750 bg-light-800 text-sm dark:border-dark-350 dark:bg-dark-250"
+                  />
+
+                  {field.value.length > 0 && (
+                    <div className="flex-start mt-2.5 gap-2.5">
+                      {field.value.map((tag, index) => (
+                        <Badge
+                          key={index}
+                          className="flex items-center gap-2 rounded-md border-none bg-light-750 px-4 py-2 text-xs text-primary-500 dark:bg-dark-250 dark:text-primary-500/80"
+                        >
+                          {tag}
+                          <span
+                            className="cursor-pointer text-sm"
+                            onClick={() => handleTagRemove(tag, field)}
+                          >
+                            <IoClose />
+                          </span>
+                        </Badge>
+                      ))}
+                    </div>
+                  )}
+                </>
               </FormControl>
               <FormDescription className="mt-2.5 text-xs text-light-500">
                 Add up to 5 tags to describe what your question is about. Start
@@ -140,6 +198,6 @@ function QuestionForm() {
       </form>
     </Form>
   );
-}
+};
 
 export default QuestionForm;
