e81d0338437677e7cc69274ec21bc532ce8d2d7d
Samson <samsonjeyanthan5@gmail.com>
2024-10-02 23:42:09 +0530

    Fix attempt - 6 - webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,17 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/home"]);
+// const isProtectedRoute = createRouteMatcher(["/home"]);
 
-export default clerkMiddleware((auth, request) => {
-  if (isProtectedRoute(request)) {
-    auth().protect();
-    console.log(process.env.CLERK_WEBHOOK_SECRET, "key-webhook");
-  }
-});
+// export default clerkMiddleware((auth, request) => {
+//   if (isProtectedRoute(request)) {
+//     auth().protect();
+//     console.log(process.env.CLERK_WEBHOOK_SECRET, "key-webhook");
+//   }
+// });
 
-export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
-};
+// export const config = {
+//   matcher: [
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//     "/(api|trpc)(.*)",
+//   ],
+// };
